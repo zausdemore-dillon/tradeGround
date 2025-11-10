fce8c5def00c052e7fa270101f3b9f0267579999
cpmcneel <connormcneel04@gmail.com>
2025-11-09 19:12:44 -0600

    created holdings api endpoint to gather tickers and prices from a users holdings

--- trades/views.py
+++ trades/views.py
@@ -1,7 +1,23 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
+from tradeGround.alpaca_request import fetch_latest_price
+from .models import Holdings
 
 def index(request):
     return HttpResponse("Hello, world. You're at the trades index.")
 
+@login_required
+def holdings_prices_api(request):
 
+    symbols = Holdings.objects.filter(user=request.user).values_list('ticker', flat=True).distinct()
+
+    data = {}
+
+    for s in symbols:
+        price = fetch_latest_price(s)
+        if price is not None:
+            data[s] = str(price)
+        else:
+            data[s] = None
+
+    return JsonResponse(data)
